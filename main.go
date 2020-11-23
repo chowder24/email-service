a0ec045efc4375d0595e24b73e536f51a9c2860f
Iamt-chadwick <akinmolayanoluwatoni@gmail.com>
2020-11-23 17:40:56 +0100

    integ with mailjet

--- main.go
+++ main.go
@@ -1,49 +1,100 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis"
+	mailjet "github.com/mailjet/mailjet-apiv3-go/v3"
+	"io/ioutil"
+	"log"
 	"time"
 )
 
-// Broker is a helper for sending and receiving messages.
-type Broker struct {
+// Sender is a helper for sending and receiving messages.
+type Sender struct {
 	client *redis.Client
 }
 
-var ctx = context.Background()
+type AppConfig struct {
+	Publickey     string `json:"MJ_APIKEY_PUBLIC"`
+	PrivateKey    string `json:"MJ_APIKEY_PRIVATE"`
+	RedisAddr     string  `json:"RedisAddr"`
+	RedisPassword  string `json:"RedisPassword"`
+}
 
-// New is a constructor for Broker.
-func New() Broker {
-	return Broker{
+var(
+	config = ReadConf("email-config.json")
+)
+
+// New is a constructor for Sender.
+func New() Sender {
+	return Sender{
 		redis.NewClient(&redis.Options{
-			Addr:     "localhost:6379",
-			Password: "",
+			Addr:    config.RedisAddr,
+			Password: config.RedisPassword,
 			DB:       0,
 		}),
 	}
 }
 
 
-func (b Broker) receiveTask(key string) string {
+func (b Sender) receiveTask(key string) string {
 	result, _ := b.client.RPop(key).Result()
 	fmt.Println(result)
 	return result
 }
 
 func handleTask(value string) {
-	fmt.Println(value)
-	fmt.Println("Sum of ", value, " is: ", value)
+	apiKey := config.Publickey
+	secretKey := config.PrivateKey
+	mj := mailjet.NewMailjetClient(apiKey, secretKey)
+	messagesInfo := []mailjet.InfoMessagesV31 {
+		mailjet.InfoMessagesV31{
+			From: &mailjet.RecipientV31{
+				Email: "akinmolayanoluwatoni@gmail.com",
+				Name: "Chowder Admin",
+			},
+			To: &mailjet.RecipientsV31{
+				mailjet.RecipientV31 {
+					Email: value,
+					Name: "passenger 1",
+				},
+			},
+			Subject: "Welcome to Chowder!",
+			TextPart: "Dear user , welcome to Chowder! May the delivery force be with you!",
+			HTMLPart: "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!",
+		},
+	}
+	messages := mailjet.MessagesV31{Info: messagesInfo }
+	res, err := mj.SendMailV31(&messages)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Data: %+v\n", res)
 }
+
 func main() {
 	TaskKey := "email:new"
-	broker := New()
+	sender := New()
 	for {
 		time.Sleep(time.Second)
-		taskValue := broker.receiveTask(TaskKey)
+		taskValue := sender.receiveTask(TaskKey)
 		if len(taskValue) > 0 {
 			go handleTask(taskValue)
 		}
 	}
+}
+
+
+func ReadConf(conf string) AppConfig {
+	data, err := ioutil.ReadFile(conf)
+	if err != nil {
+		panic(err)
+	}
+	obj := AppConfig{}
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		panic(err)
+	}
+	return obj
 }
